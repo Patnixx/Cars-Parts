4ced9fbb83fedb5c948431bb0cf94ac690f5f9d8
patrik <patkonemcok@gmail.com>
2025-11-01 21:53:02 +0100

    [21:52] - Added models & migrations

---
+++ app/Models/Car.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'registration_number',
+        'is_registered'
+    ];
+
+    protected $casts = [
+        'is_registered' => 'boolean',
+    ];
+
+    public function parts()
+    {
+        return $this->hasMany(Part::class);
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,9 @@ import './bootstrap';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+// Import Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).mount('#app');
